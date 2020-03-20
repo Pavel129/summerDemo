4c336bbb3404edd0e8ee337f22610c561d1706a2
Pavel Chekov <chekov_pavel_@mail.ru>
2020-03-20 19:38:48 +0300

    added project

---
+++ Alerts_BWSwift/Alerts_BWSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alerts_BWSwift
+//
+//  Created by Pavel Chekov on 20/03/2020.
+//  Copyright © 2020 Pavel Chekov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
